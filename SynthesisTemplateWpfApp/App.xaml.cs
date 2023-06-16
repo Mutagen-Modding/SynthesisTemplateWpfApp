b12b751d16c0b5745d62f83c1d33fe350374c45c
Justin Swanson <justin.c.swanson@gmail.com>
2023-06-16 00:52:59 -0500

    Basic Synthesis bootstrap callbacks

--- SynthesisTemplateWpfApp/App.xaml.cs
+++ SynthesisTemplateWpfApp/App.xaml.cs
@@ -1,10 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 using System.Windows;
+using Mutagen.Bethesda.Skyrim;
+using Mutagen.Bethesda.Synthesis;
 
 namespace SynthesisTemplateWpfApp
 {
@@ -13,5 +10,47 @@ namespace SynthesisTemplateWpfApp
     /// </summary>
     public partial class App : Application
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            SynthesisPipeline.Instance
+                .AddPatch<ISkyrimMod, ISkyrimModGetter>(RunPatch)
+                .SetTypicalOpen(StandaloneOpen)
+                .SetOpenForSettings(OpenForSettings)
+                .SetForWpf()
+                .Run(e.Args)
+                .Wait();
+        }
+
+        /// <summary>
+        /// The normal Synthesis entry point
+        /// Will be called by Synthesis when users want to patch
+        /// </summary>
+        private async Task RunPatch(IPatcherState<ISkyrimMod, ISkyrimModGetter> state)
+        {
+        }
+
+        /// <summary>
+        /// Will be called when users double click the exe, or is run without any args from the IDE
+        /// </summary>
+        public int StandaloneOpen()
+        {
+            var window = new MainWindow();
+            window.Show();
+            return 0;
+        }
+
+        /// <summary>
+        /// Will be called when users request the app to show its settings
+        /// Very similar to StandaloneOpen, but you might opt to hide any "Start Patch" buttons
+        /// that you might show when run truly standalone.
+        /// </summary>
+        public int OpenForSettings(IOpenForSettingsState state)
+        {
+            var window = new MainWindow();
+            window.Show();
+            return 0;
+        }
     }
 }
