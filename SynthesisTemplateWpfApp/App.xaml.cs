d6c7935bdff20786d23111564efa5aad09192940
Justin Swanson <justin.c.swanson@gmail.com>
2023-06-16 10:24:40 -0500

    Some organization

--- SynthesisTemplateWpfApp/App.xaml.cs
+++ SynthesisTemplateWpfApp/App.xaml.cs
@@ -2,6 +2,7 @@
 using System.Windows;
 using Mutagen.Bethesda.Skyrim;
 using Mutagen.Bethesda.Synthesis;
+using SynthesisTemplateWpfApp.Engine;
 
 namespace SynthesisTemplateWpfApp
 {
@@ -10,13 +11,19 @@ namespace SynthesisTemplateWpfApp
     /// </summary>
     public partial class App : Application
     {
+        /// <summary>
+        /// The WPF equivalent of a "main" function
+        /// </summary>
         protected override void OnStartup(StartupEventArgs e)
         {
             base.OnStartup(e);
 
+            // Call into Synthesis to parse the args and decide which mode to start in
             SynthesisPipeline.Instance
                 .AddPatch<ISkyrimMod, ISkyrimModGetter>(RunPatch)
                 .SetTypicalOpen(StandaloneOpen)
+                // Alternatively can use this TypicalOpen to have it just run patcher if exe is opened
+                //.SetTypicalOpen(GameRelease.SkyrimSE, "SomeModName.esp")
                 .SetOpenForSettings(OpenForSettings)
                 .SetForWpf()
                 .Run(e.Args)
@@ -29,12 +36,16 @@ namespace SynthesisTemplateWpfApp
         /// </summary>
         private async Task RunPatch(IPatcherState<ISkyrimMod, ISkyrimModGetter> state)
         {
+            // Have the run patch code exist in another file for organization
+            await new RunPatchLogic().Run(state);
         }
 
         /// <summary>
-        /// Will be called when users double click the exe, or is run without any args from the IDE
+        /// Will be called when users request the app to show its settings
+        /// Very similar to StandaloneOpen, but you might opt to hide any "Start Patch" buttons as it will be running
+        /// as a subwindow of Synthesis just for the settings
         /// </summary>
-        public int StandaloneOpen()
+        public int OpenForSettings(IOpenForSettingsState state)
         {
             var window = new MainWindow();
             window.Show();
@@ -42,11 +53,10 @@ namespace SynthesisTemplateWpfApp
         }
 
         /// <summary>
-        /// Will be called when users request the app to show its settings
-        /// Very similar to StandaloneOpen, but you might opt to hide any "Start Patch" buttons
-        /// that you might show when run truly standalone.
+        /// Will be called when users double click the exe, or is run without any args from the IDE.
+        /// This is meant to be used if you want the app to open a window if run standalone.
         /// </summary>
-        public int OpenForSettings(IOpenForSettingsState state)
+        public int StandaloneOpen()
         {
             var window = new MainWindow();
             window.Show();
---
+++ SynthesisTemplateWpfApp/Engine/RunPatchLogic.cs
@@ -0,0 +1,13 @@
+﻿using System.Threading.Tasks;
+using Mutagen.Bethesda.Skyrim;
+using Mutagen.Bethesda.Synthesis;
+
+namespace SynthesisTemplateWpfApp.Engine;
+
+public class RunPatchLogic
+{
+    public async Task Run(IPatcherState<ISkyrimMod, ISkyrimModGetter> state)
+    {
+        // Normal patcher logic you would have in a patcher without a custom UI
+    }
+}
