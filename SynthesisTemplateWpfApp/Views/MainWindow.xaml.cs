3586e99a81dff4be3b276a25bdb0a3f9b040a3a6
Justin Swanson <justin.c.swanson@gmail.com>
2023-06-18 01:47:53 -0500

    Add MahApps styling with Steel color choice

--- SynthesisTemplateWpfApp/Views/MainWindow.xaml.cs
+++ SynthesisTemplateWpfApp/Views/MainWindow.xaml.cs
@@ -1,12 +1,11 @@
-﻿using System.Windows;
-using SynthesisTemplateWpfApp.ViewModels.Singletons;
+﻿using SynthesisTemplateWpfApp.ViewModels.Singletons;
 
 namespace SynthesisTemplateWpfApp.Views
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow
     {
         public MainWindow(MainVm mvm)
         {
